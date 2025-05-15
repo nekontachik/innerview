4d358844658d4cda3bd0bbc1142345dcb30c91df
nekontachik <alex.dzhos@gmail.com>
2025-05-15 09:06:13 +0300

    chore: mark deploy-fix climb as complete (all moves done)

--- src/lib/bivvy/middleware/index.ts
+++
@@ -1,6 +0,0 @@
-export { rateLimit } from './rateLimit';
-export { validateRequest } from './validateRequest';
-export { errorHandler } from './errorHandler';
-export { logger } from './logger';
-export { compressResponse } from './compress';
-export { cacheResponse } from './cache'; 
